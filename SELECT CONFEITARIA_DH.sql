56a3a14750e505180aeeb100eae1d00622c9f1d7
RicardoCTD <rosastaricardo@gmail.com>
2021-09-19 04:51:33 -0300

    campo select

--- SELECT CONFEITARIA_DH.sql
+++ SELECT CONFEITARIA_DH.sql
@@ -1,1 +1,10 @@
-select * from cliente
+select * from cliente;
+
+
+-- usando o join para conectar as tabelas que têm relações e obter a informação desejada, que neste caso, é saber o valor total do pedido 4
+
+select KIT.ID_PEDIDO, kit.TIPO_KIT, kit.TAMANHO_KIT, sum(kit.PRECO_KIT) valor_total_pedidos , produto.TIPO , produto.QUANTIDADE FROM kit 
+INNER JOIN kit_produto ON KIT_PRODUTO.ID_KIT = kit.ID_KIT
+INNER JOIN produto ON produto.ID_PRODUTO = kit_produto.ID_PRODUTO
+group by ID_PEDIDO having ID_PEDIDO = 4;
+ 
